c7d09a791f718bd820e154072485fe7edb136332
sudo97 <i.mihnevich@gmail.com>
2024-07-15 14:38:04 +0300

    refinements

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,18 @@ module Main where
 
 import Asset
 import Control.Monad
+-- import Language.JavaScript.Parser (renderToString)
+
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Except
 import qualified Data.Map as M
-import Data.Maybe (catMaybes)
-import qualified Data.Set as S
--- import Language.JavaScript.Parser (renderToString)
-import System.Directory
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
-import System.FilePath
+
+-- import Data.IORef
+-- import Control.Monad.Error.Class
+-- import System.Directory
+-- import System.FilePath
 
 main :: IO ()
 main = do
@@ -27,39 +29,12 @@ main = do
 printGraph :: String -> GraphM ()
 printGraph path = do
   graph <- createGraph path
-  let maps = aMapping <$> graph
-  let mapStrings = M.toList <$> maps
-  liftIO $ print mapStrings
-  pure ()
+  forM_ graph $ \(asset, mapping) -> liftIO $ do
+    putStrLn $ aPath asset
+    print mapping
+    putStrLn ""
 
 type GraphM a = ExceptT String IO a
 
-createGraph :: String -> GraphM [Asset]
-createGraph path = do
-  content <- liftIO $ readFile path
-  mainAsset <- ExceptT $ return $ mkAsset path content 0
-  let initialVisited = S.singleton (aPath mainAsset)
-  let initialQueue = [(mainAsset, 0)]
-  (mainAsset :) <$> traverseGraph initialQueue initialVisited []
-
-traverseGraph :: [(Asset, Int)] -> S.Set String -> [Asset] -> GraphM [Asset]
-traverseGraph [] _ result = return result
-traverseGraph ((asset, id') : queue) visited result = do
-  let dirname = takeDirectory (aPath asset)
-  -- liftIO $ putStrLn dirname
-  newAssets <- forM (aDependencies asset) $ \dep -> do
-    let depPath = dirname </> dep
-    absolutePath <- liftIO $ canonicalizePath depPath
-    -- liftIO $ putStrLn depPath
-    -- liftIO $ putStrLn absolutePath
-    if S.member absolutePath visited
-      then return Nothing
-      else do
-        content <- liftIO $ readFile absolutePath
-        newAsset <- ExceptT $ return $ mkAsset absolutePath content (id' + 1)
-        return $ Just (newAsset, id' + 1)
-  let newAssetsFiltered = catMaybes newAssets
-  let newVisited = foldr (S.insert . aPath . fst) visited newAssetsFiltered
-  let newQueue = queue ++ newAssetsFiltered
-  let updatedAsset = asset {aMapping = M.fromList [(dep, aId newAsset) | (newAsset, _) <- newAssetsFiltered, dep <- aDependencies asset]}
-  traverseGraph newQueue newVisited (result ++ [updatedAsset])
+createGraph :: String -> GraphM [(Asset, M.Map String Int)]
+createGraph = undefined
--- lib/Asset.hs
+++ lib/Asset.hs
@@ -1,6 +1,5 @@
 module Asset (Asset (..), mkAsset) where
 
-import qualified Data.Map as M
 import Language.JavaScript.Parser
 import Transpile
 
@@ -8,7 +7,7 @@ data Asset = Asset
   { aId :: Int,
     aPath :: String,
     aContent :: JSAST,
-    aMapping :: M.Map String Int,
+    -- aMapping :: M.Map String Int,
     aDependencies :: [String]
   }
   deriving (Show, Eq)
@@ -17,4 +16,4 @@ mkAsset :: String -> String -> Int -> Either String Asset
 mkAsset path content id' = do
   mdl <- parseModule content path
   deps <- imports mdl
-  pure Asset {aId = id', aPath = path, aContent = transpile mdl, aMapping = M.empty, aDependencies = deps}
+  pure Asset {aId = id', aPath = path, aContent = transpile mdl, aDependencies = deps}
